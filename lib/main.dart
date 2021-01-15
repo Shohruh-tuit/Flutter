33abbe9a3969b50577f2748217a3a2d11bfb7edf
Shohruh Masharipov <shohruhmasharipov49@gmail.com>
2021-01-15 18:08:17 +0500

    Right now

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-      MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Center(child: Text('Lesson 1'),) ,
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        appBar: AppBar(
+          title: Center(
+            child: Text('Lesson 1'),
+          ),
+          backgroundColor: Colors.lightBlueAccent,
+        ),
+        /**/
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'),
           ),
         ),
       ),
+    ),
   );
 }
