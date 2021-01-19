c4b0420c623bf42cc49271a52d27c25cabcf3768
Shohruh Masharipov <shohruhmasharipov49@gmail.com>
2021-01-19 09:54:43 +0500

    yaxshi bovotti

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,43 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.amberAccent,
+//         appBar: AppBar(
+//           title: Center(
+//             child: Text('Lesson 1'),
+//           ),
+//           backgroundColor: Colors.lightBlueAccent,
+//         ),
+//         /**/
+//         body: Center(
+//           child: Image(
+//             image: AssetImage('images/love.png')
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amberAccent,
-        appBar: AppBar(
-          title: Center(
-            child: Text('Lesson 1'),
-          ),
-          backgroundColor: Colors.lightBlueAccent,
-        ),
-        /**/
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-                'https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg'),
+        body: SafeArea(
+          child: Container(
+            child: Text(
+              'Hello my dream',
+              style: TextStyle(fontSize: 50, color: Colors.white),
+            ),
+            padding: EdgeInsets.only(top: 50, left: 15),
+            margin: EdgeInsets.all(8),
+            color: Colors.lightGreen,
+            height: 300,
+            width: 400,
           ),
         ),
       ),
